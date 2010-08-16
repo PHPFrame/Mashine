f296a69047e0f90b5a6d1171f07f7b7353134bc6
Lupo Montero <lupo@e-noise.com>
2010-08-16 01:46:06 +0100

    Show sitemap in menu

--- src/views/partials/menu.php
+++ src/views/partials/menu.php
@@ -1,5 +1,5 @@
 <?php if ($session->isAuth()) : ?>
-[cms:depth=1&show_root_as_child=true&exclude=sitemap,user/login,user/signup]
+[cms:depth=1&show_root_as_child=true&exclude=user/login,user/signup]
 <?php else : ?>
-[cms:depth=1&show_root_as_child=true&exclude=sitemap,user/logout]
+[cms:depth=1&show_root_as_child=true&exclude=user/logout]
 <?php endif; ?>
