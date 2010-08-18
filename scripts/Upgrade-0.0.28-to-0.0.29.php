1320c328f15f9eaff5d06a29daf0947c8e508dac
Lupo Montero <lupo@e-noise.com>
2010-08-18 13:34:46 +0100

    Tweaking update script

--- scripts/Upgrade-0.0.28-to-0.0.29.php
+++ scripts/Upgrade-0.0.28-to-0.0.29.php
@@ -1,11 +1,12 @@
 <?php
 class Upgrade_0_0_28_to_0_0_29
 {
-    private $_app;
+    private $_app, $_install_dir;
 
     public function __construct(PHPFrame_Application $app)
     {
         $this->_app = $app;
+        $this->_install_dir = $this->app()->getInstallDir();
     }
 
     public function app()
@@ -15,13 +16,23 @@ class Upgrade_0_0_28_to_0_0_29
 
     public function run()
     {
+        $this->_removeObsoleteCmsController();
         $this->_moveCmsViews();
         $this->_renameCmsControllerInMVCContent();
+        $this->_updateLinkToMashineFeed();
+    }
+
+    private function _removeObsoleteCmsController()
+    {
+        $cms_controller = $this->_install_dir.DS."src".DS."controllers".DS."cms.php";
+        if (is_file($cms_controller)) {
+            PHPFrame_Filesystem::rm($cms_controller);
+        }
     }
 
     private function _moveCmsViews()
     {
-        $views_dir = $this->app()->getInstallDir().DS."src".DS."views";
+        $views_dir = $this->_install_dir.DS."src".DS."views";
         PHPFrame_Filesystem::cp($views_dir.DS."cms/", $views_dir, true);
         PHPFrame_Filesystem::rm($views_dir.DS."cms", true);
     }
@@ -52,6 +63,26 @@ class Upgrade_0_0_28_to_0_0_29
                     $mapper->insert($item);
                 }
             }
+        }
+    }
+
+    private function _updateLinkToMashineFeed()
+    {
+        $mapper = new ContentMapper(
+            $this->app()->db(), $this->app()->getTmpDir().DS."cms"
+        );
+
+        $old_url = "http://github.com/lupomontero/Mashine/commits/master.atom";
+        $content = $mapper->findOne("mashine-on-github");
+        if ($content instanceof FeedContent
+            && $content->param("feed_url") == $old_url
+        ) {
+            $content->param(
+                "feed_url",
+                "http://github.com/E-NOISE/Mashine/commits/master.atom"
+            );
+
+            $mapper->insert($content);
         }
     }
 }
