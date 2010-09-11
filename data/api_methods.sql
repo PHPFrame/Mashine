672a2026c55230722e19a9549d35686b8043f425
Lupo Montero <lupo@e-noise.com>
2010-09-11 18:19:10 +0100

    Fixed new api_methods.sql file so that it can be processed by the installer.

--- data/api_methods.sql
+++ data/api_methods.sql
@@ -1,18 +1,17 @@
-INSERT INTO `api_methods` (`id`, `method`, `oauth`, `cookie`) VALUES
-(1, 'session/login', '2', '2'),
-(2, 'session/logout', '0', '1'),
-(3, 'oauth/save_method_auth', '0', '2'),
-(4, 'users/get', '1', '1'),
-(5, 'users/post', '1', '2'),
-(6, 'content/get', '1', '1'),
-(7, 'content/post', '1', '2'),
-(8, 'content/delete', '1', '1'),
-(9, 'notifications/get', '0', '1'),
-(10, 'notifications/delete', '0', '1'),
-(11, 'plugins/get', '0', '1'),
-(12, 'plugins/post', '0', '2'),
-(13, 'plugins/delete', '0', '1'),
-(14, 'users/delete', '2', '1'),
-(15, 'users/search', '1', '1'),
-(16, 'oauth/access_token', '0', '1'),
-(17, 'oauth/request_token', '0', '1');
+INSERT INTO `api_methods` VALUES (1, 'session/login', '2', '2');
+INSERT INTO `api_methods` VALUES (2, 'session/logout', '0', '1');
+INSERT INTO `api_methods` VALUES (3, 'oauth/save_method_auth', '0', '2');
+INSERT INTO `api_methods` VALUES (4, 'users/get', '1', '1');
+INSERT INTO `api_methods` VALUES (5, 'users/post', '1', '2');
+INSERT INTO `api_methods` VALUES (6, 'content/get', '1', '1');
+INSERT INTO `api_methods` VALUES (7, 'content/post', '1', '2');
+INSERT INTO `api_methods` VALUES (8, 'content/delete', '1', '1');
+INSERT INTO `api_methods` VALUES (9, 'notifications/get', '0', '1');
+INSERT INTO `api_methods` VALUES (10, 'notifications/delete', '0', '1');
+INSERT INTO `api_methods` VALUES (11, 'plugins/get', '0', '1');
+INSERT INTO `api_methods` VALUES (12, 'plugins/post', '0', '2');
+INSERT INTO `api_methods` VALUES (13, 'plugins/delete', '0', '1');
+INSERT INTO `api_methods` VALUES (14, 'users/delete', '2', '1');
+INSERT INTO `api_methods` VALUES (15, 'users/search', '1', '1');
+INSERT INTO `api_methods` VALUES (16, 'oauth/access_token', '0', '1');
+INSERT INTO `api_methods` VALUES (17, 'oauth/request_token', '0', '1');
