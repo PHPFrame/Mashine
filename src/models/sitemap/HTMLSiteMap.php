a8d52e28356afde4dcac129533f16d064e49ee82
Lupo Montero <lupo@e-noise.com>
2010-10-27 23:52:52 +0800

    Added slugs as classes in the links produced by HTMLSitemap. This is useful for styling or selecting each item if needed.

--- src/models/sitemap/HTMLSiteMap.php
+++ src/models/sitemap/HTMLSiteMap.php
@@ -74,6 +74,7 @@ class HTMLSiteMap
             }
         }
 
+        $slug = $this->_node->slug();
         $str = "";
 
         if (!$this->showRoot()) {
@@ -82,11 +83,11 @@ class HTMLSiteMap
             }
         } elseif ($this->showRootAsChild()) {
             $str .= "    <li>\n";
-            $str .= "        <a href=\"".$this->_node->slug()."\"";
+            $str .= "        <a href=\"".$slug."\" class=\"".$slug;
             if ($this->_node->active()) {
-                $str .= " class=\"active\"";
+                $str .= " active";
             }
-            $str .= ">".$this->_node->shortTitle()."</a>\n";
+            $str .= "\">".$this->_node->shortTitle()."</a>\n";
             $str .= "    </li>\n";
             foreach ($this->_node->getChildren() as $child) {
                 $str .= $this->_nodeToHTML($child);
@@ -248,13 +249,13 @@ class HTMLSiteMap
         }
 
         $str  = $indent."<li>\n";
-        $str .= $indent."    <a href=\"".$node_url."\"";
+        $str .= $indent."    <a href=\"".$node_url."\" class=\"".$node_url;
         if ($node->active()) {
-            $str .= " class=\"active\"";
+            $str .= " active";
         } elseif ($node->activeParent()) {
-            $str .= " class=\"parent\"";
+            $str .= " parent";
         }
-        $str .= ">".$node_title."</a>\n";
+        $str .= "\">".$node_title."</a>\n";
 
         if ($node->hasChildren()) {
             $depth++;
