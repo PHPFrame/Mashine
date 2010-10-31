43103ded7ca936e4e018ec552c2a66ee9e623970
Lupo Montero <lupo@e-noise.com>
2010-10-31 20:56:19 +0000

    Added SyntaxHighlighter plugin

--- public/themes/default/index.php
+++ public/themes/default/index.php
@@ -16,11 +16,7 @@ $content   = $request->param("_content_active");
 // Add Javascript and CSS
 $this->addScript("http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
 $this->addScript($base_url."assets/js/modernizr-1.5.min.js");
-// $this->addScript("http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js");
-// $this->addScript("http://ajax.microsoft.com/ajax/jquery.validate/1.6/jquery.validate.pack.js");
 $this->addStyleSheet($base_url."assets/css/mashine.css");
-$this->addStyleSheet($base_url."assets/css/syntaxhighlighter/shCore.css");
-$this->addStyleSheet($base_url."assets/css/syntaxhighlighter/shThemeRDark.css");
 
 if ($user->groupId() > 0 && $user->groupId() <= 2) {
     $this->addStyleSheet($base_url."assets/css/mashine.user.css");
@@ -57,11 +53,9 @@ $this->addStyleSheet($base_url."themes/default/css/styles.css");
 </div><!-- #wrapper -->
 
 <footer id="footer">
-<small id="copyright">
-<p>
-Powered by <?php echo nl2br(PHPFrame::version())."\n"; ?>
-</p>
-</small>
+  <small id="copyright">
+    <p>Powered by <?php echo nl2br(PHPFrame::version())."\n"; ?></p>
+  </small>
 </footer>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
@@ -71,35 +65,19 @@ Powered by <?php echo nl2br(PHPFrame::version())."\n"; ?>
 <?php if ($user->groupId() > 0 && $user->groupId() <= 2) : ?>
 <script src="<?php echo $base_url; ?>assets/js/mashine.user.js"></script>
 <?php endif; ?>
-<script src="<?php echo $base_url; ?>assets/js/syntaxhighlighter/shCore.js"></script>
-<script src="<?php echo $base_url; ?>assets/js/syntaxhighlighter/shAutoloader.js"></script>
 <script>
 jQuery(document).ready(function() {
-    try { EN({ debug: false }); } catch(e) { alert(e); }
+  try { EN({ debug: false }); } catch(e) { alert(e); }
 
-    EN.initToolTips('.tooltip');
-    EN.confirm('.confirm');
-    EN.validate('.validate');
+  EN.initToolTips('.tooltip');
+  EN.confirm('.confirm');
+  EN.validate('.validate');
 
-    // close sysevent boxes
-    jQuery('a.close_button').live('click', function(e) {
-        e.preventDefault();
-        jQuery(this).closest('.sysevent').fadeOut('1500');
-    });
-
-    SyntaxHighlighter.autoloader(
-      'js jscript javascript assets/js/syntaxhighlighter/shBrushJScript.js',
-      'php assets/js/syntaxhighlighter/shBrushPhp.js',
-      'css assets/js/syntaxhighlighter/shBrushCss.js',
-      'bash shell assets/js/syntaxhighlighter/shBrushBash.js',
-      'plain assets/js/syntaxhighlighter/shBrushPlain.js',
-      'python assets/js/syntaxhighlighter/shBrushPython.js',
-      'ruby assets/js/syntaxhighlighter/shBrushRuby.js',
-      'sql assets/js/syntaxhighlighter/shBrushSql.js',
-      'xml html assets/js/syntaxhighlighter/shBrushXml.js'
-    );
-
-    SyntaxHighlighter.all();
+  // close sysevent boxes
+  jQuery('a.close_button').live('click', function(e) {
+    e.preventDefault();
+    jQuery(this).closest('.sysevent').fadeOut('1500');
+  });
 });
 </script>
 
--- src/views/admin/plugins/index.php
+++ src/views/admin/plugins/index.php
@@ -1,48 +1,53 @@
 <header id="content-header">
-    <h1><?php echo $title; ?></h1>
+  <h1><?php echo $title; ?></h1>
 </header>
 
 <div id="content-body">
 
+<ul class="list-filter">
+  <li><a href="admin/plugins?f=active">Active</a></li>
+  <li><a href="admin/plugins?f=disabled">Disabled</a></li>
+  <li><a href="admin/plugins?f=all">All</a></li>
+</ul>
+
 <table>
-    <thead>
-        <tr>
-            <th>Name:</th>
-            <th>Summary:</th>
-            <th>Author:</th>
-            <th>Version:</th>
-            <th>Enabled:</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($plugins as $plugin) : ?>
-        <tr>
-            <td><?php echo $plugin->name(); ?></td>
-            <td><?php echo $plugin->summary(); ?></td>
-            <td><?php echo $plugin->author(); ?></td>
-            <td><?php echo $plugin->version(); ?></td>
-            <td><?php echo ($plugin->enabled()) ? "Yes" : "No"; ?></td>
-            <td>
-                <a href="admin/plugins/options?id=<?php echo $plugin->id(); ?>">
-                    Options
-                </a>
-                <?php if ($plugin->id() > 1): ?>
-                 -
-                <?php if ($plugin->enabled()) : ?>
-                    <a href="plugins/disable?id=<?php echo $plugin->id(); ?>">Disable</a>
-                <?php else : ?>
-                    <a href="plugins/enable?id=<?php echo $plugin->id(); ?>">Enable</a>
-                <?php endif; ?>
-                <!--
-                 -
-                <a href="plugins/uninstall?id=<?php echo $plugin->id(); ?>">Uninstall</a>
-                -->
-                <?php endif ?>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </tbody>
+  <thead>
+    <tr>
+      <th>Name:</th>
+      <th>Summary:</th>
+      <th>Author:</th>
+      <th>Version:</th>
+      <th>Enabled:</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($plugins as $plugin) : ?>
+    <tr>
+      <td><?php echo $plugin->name(); ?></td>
+      <td><?php echo $plugin->summary(); ?></td>
+      <td><?php echo $plugin->author(); ?></td>
+      <td><?php echo $plugin->version(); ?></td>
+      <td><?php echo ($plugin->enabled()) ? "Yes" : "No"; ?></td>
+      <td>
+        <a href="admin/plugins/options?id=<?php echo $plugin->id(); ?>">Options</a>
+        <?php if ($plugin->id() > 1): ?>
+         -
+        <?php if ($plugin->enabled()) : ?>
+          <a href="plugins/disable?id=<?php echo $plugin->id(); ?>">Disable</a>
+        <?php else : ?>
+          <a href="plugins/enable?id=<?php echo $plugin->id(); ?>">Enable</a>
+        <?php endif; ?>
+        <!--
+         -
+        <a href="plugins/uninstall?id=<?php echo $plugin->id(); ?>">Uninstall</a>
+        -->
+        <?php endif ?>
+      </td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
 </table>
 
 </div><!-- #content-body -->
+
