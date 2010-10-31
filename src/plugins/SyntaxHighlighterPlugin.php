9119b6ae5e201856623673bc6a57fdc1b0df1dec
Lupo Montero <lupo@e-noise.com>
2010-11-01 04:56:19 +0800

    Added SyntaxHighlighter plugin

---
+++ src/plugins/SyntaxHighlighterPlugin.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * src/plugins/SyntaxHighlighterPlugin.php
+ *
+ * PHP version 5
+ *
+ * @category  PHPFrame_Applications
+ * @package   Mashine
+ * @author    Lupo Montero <lupo@e-noise.com>
+ * @copyright 2010 E-NOISE.COM LIMITED
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      https://github.com/lupomontero/Mashine
+ */
+
+/**
+ * SyntaxHighlighterPlugin class
+ *
+ * @category PHPFrame_Applications
+ * @package  Mashine
+ * @author   Lupo Montero <lupo@e-noise.com>
+ * @license  http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link     https://github.com/lupomontero/Mashine
+ * @since    1.0
+ */
+class SyntaxHighlighterPlugin extends AbstractPlugin
+{
+    /**
+     * Constructor.
+     *
+     * @param PHPFrame_Application $app Instance of application object.
+     *
+     * @return void
+     * @since  1.0
+     */
+    public function __construct(PHPFrame_Application $app)
+    {
+        parent::__construct($app);
+    }
+
+    /**
+     * Install plugin.
+     *
+     * @return void
+     * @since  1.0
+     */
+    public function install()
+    {
+        $this->options[$this->getOptionsPrefix()."version"] = "1.0";
+    }
+
+    public function preApplyTheme()
+    {
+        $base_url = $this->app()->config()->get("base_url");
+        $document = $this->app()->response()->document();
+        $document->addStyleSheet($base_url."assets/css/syntaxhighlighter/shCore.css");
+        $document->addStyleSheet($base_url."assets/css/syntaxhighlighter/shThemeRDark.css");
+    }
+
+    public function postApplyTheme()
+    {
+        $base_url = $this->app()->config()->get("base_url");
+        $document = $this->app()->response()->document();
+
+        ob_start();
+        ?>
+
+<script src="<?php echo $base_url; ?>assets/js/syntaxhighlighter/shCore.js"></script>
+<script src="<?php echo $base_url; ?>assets/js/syntaxhighlighter/shAutoloader.js"></script>
+
+<script>
+jQuery(document).ready(function() {
+  SyntaxHighlighter.autoloader(
+    'js jscript javascript assets/js/syntaxhighlighter/shBrushJScript.js',
+    'php assets/js/syntaxhighlighter/shBrushPhp.js',
+    'css assets/js/syntaxhighlighter/shBrushCss.js',
+    'bash shell assets/js/syntaxhighlighter/shBrushBash.js',
+    'plain assets/js/syntaxhighlighter/shBrushPlain.js',
+    'python assets/js/syntaxhighlighter/shBrushPython.js',
+    'ruby assets/js/syntaxhighlighter/shBrushRuby.js',
+    'sql assets/js/syntaxhighlighter/shBrushSql.js',
+    'xml html assets/js/syntaxhighlighter/shBrushXml.js'
+  );
+
+  SyntaxHighlighter.all();
+});
+</script>
+
+        <?php
+        $document->appendBody(ob_get_clean());
+    }
+}
+
