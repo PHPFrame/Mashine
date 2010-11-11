4107a0ca11ce76a65a32fd6e4cee1ab3144fbcf4
Lupo Montero <lupo@e-noise.com>
2010-11-12 04:01:39 +0800

    Standardising HTML indentation

--- src/views/user/dashboard.php
+++ src/views/user/dashboard.php
@@ -1,19 +1,20 @@
 <header id="content-header">
-    <h1><?php echo $title; ?></h1>
+  <h1><?php echo $title; ?></h1>
 </header>
 
 <div id="content-body">
 
 <?php foreach ($dashboard_boxes as $box) : ?>
 <div class="dashboard-box-outer">
-    <div class="dashboard-box-inner">
-        <h3><?php echo $box["title"]; ?></h3>
-        <div class="dashboard-box-content">
-            <?php echo $box["body"]; ?>
-        </div>
+  <div class="dashboard-box-inner">
+    <h3><?php echo $box["title"]; ?></h3>
+    <div class="dashboard-box-content">
+      <?php echo $box["body"]; ?>
     </div>
+  </div>
 </div>
 <?php endforeach; ?>
 
-</div><!-- #content-body -->
+<div style="clear: left;"></div>
 
+</div><!-- #content-body -->
