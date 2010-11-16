80777f7d34b05dbf5f60d7d34c3e367190fb346a
Lupo Montero <lupo@e-noise.com>
2010-11-16 14:29:34 +0000

    Cleaning up indentation in views...

--- src/views/feed.php
+++ src/views/feed.php
@@ -1,13 +1,13 @@
 <header id="content-header">
-    <h1><?php echo $content->title(); ?></h1>
-    <?php echo $content->editLink($user); ?>
-    <p class="feed-info">
-        Feed URL: <a href="<?php echo $content->link(); ?>"><?php echo $content->link(); ?></a><br />
-        <?php $description = $content->description(); ?>
-        <?php echo ($description) ? "Description: ".$description : ""; ?>
-        <?php $img = $helper->displayFeedImage($content); ?>
-        <?php echo ($img) ? "Image: ".$img : ""; ?>
-    </p>
+  <h1><?php echo $content->title(); ?></h1>
+  <?php echo $content->editLink($user); ?>
+  <p class="feed-info">
+    Feed URL: <a href="<?php echo $content->link(); ?>"><?php echo $content->link(); ?></a><br />
+    <?php $description = $content->description(); ?>
+    <?php echo ($description) ? "Description: ".$description : ""; ?>
+    <?php $img = $helper->displayFeedImage($content); ?>
+    <?php echo ($img) ? "Image: ".$img : ""; ?>
+  </p>
 </header>
 
 <div id="content-body" class="feed">
@@ -17,13 +17,13 @@
 
 <header>
 <h2 class="post-title">
-    <a href="<?php echo $item["link"]; ?>">
-        <?php echo $item["title"]; ?>
-    </a>
+  <a href="<?php echo $item["link"]; ?>">
+    <?php echo $item["title"]; ?>
+  </a>
 </h2>
 <p class="post-info">
-    Posted by <a href="#"><?php echo $item["author"]; ?></a>
-    on <time datetime="<?php echo date("Y-m-d\TH:i", strtotime($item["pub_date"])); ?>" pubdate><?php echo date("l jS F Y", strtotime($item["pub_date"])); ?><time>
+  Posted by <a href="#"><?php echo $item["author"]; ?></a>
+  on <time datetime="<?php echo date("Y-m-d\TH:i", strtotime($item["pub_date"])); ?>" pubdate><?php echo date("l jS F Y", strtotime($item["pub_date"])); ?><time>
 </p>
 </header>
 
