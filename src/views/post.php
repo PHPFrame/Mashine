7e18edb04f25601db9de7b050c4065b431c4e048
Lupo Montero <lupo@e-noise.com>
2011-03-14 03:54:11 +0800

    Added 'share' links in posts and post views as part of social plugin

--- src/views/post.php
+++ src/views/post.php
@@ -1,6 +1,10 @@
 <header id="content-header">
   <h1><?php echo $content->title(); ?></h1>
   <?php echo $content->editLink($user); ?>
+  <p class="post-info">
+    Posted by <?php echo $content->author(); ?>
+    on <time datetime="<?php echo date("Y-m-d\TH:i", strtotime($content->pubDate())); ?>" pubdate><?php echo date("l jS F Y", strtotime($content->pubDate())); ?></time>
+  </p>
 </header>
 
 <div id="content-body" class="post-body">
